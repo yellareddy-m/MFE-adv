ae336757f932e007fba9430173fd63cb6482e475
Yella Reddy <m.yellareddy6@gmail.com>
2025-12-13 15:01:37 +0530

    fix routing

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,15 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-const mount = (ele) => {
-  ReactDOM.render(<App />, ele);
+const mount = (ele, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, ele);
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
